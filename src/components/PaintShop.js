50c9134612f8b03b58464d879e16c236e766f7da
Jaysig <jasontsigmon@gmail.com>
2017-08-28 12:03:44 -0500

    Style cleanup

--- src/components/PaintShop.js
+++ src/components/PaintShop.js
@@ -2,38 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import * as actions from '../actions/paints';
+import * as styles from './paintShopStyle';
 
-const block = {
-  position: 'absolute',
-  height: '2em',
-  width: '600px',
-  'text-align': 'center',
-  'line-height': '2em'
-
-}
-
-const sign = {
-  'margin-left': '30%',
-  width: '40%',
-  background: '#666',
-  'text-shadow': '0px 1px 0px rgba(255,255,255,.5)',
-  opacity: '30%'
-}
-
-const cube = {
-  'font-size': '3em',
-  width: '2em',
-  margin: '1em auto',
-  'transform-style': 'preserve-3d',
-  transform: 'rotateX(-10deg) rotateY(1deg)',
-}
-const row = {
-  margin: '10px',
-  'font-family': 'Lato, sans-serif',
-  'margin-left': '10%',
-  width: '400px',
-  'justify-content': 'space-between'
-}
 const daytime = {
   background: '#F6F6F6',
   color: 'black'
@@ -41,31 +11,7 @@ const daytime = {
 const nighttime = {
   background: '#111'
 }
-const update = {
-  color: 'white',
-  border: 'orange solid 2px',
-  width: '25%',
-  padding: '1%',
-  'text-align': 'center',
-  height: '40px'
-}
-const door = {
-  background: 'url("http://dc428.4shared.com/img/-vayshJ-/s3/ClassDoor.png")',
-  'background-repeat': 'no-repeat',
-  'background-position': 'center center',
-  color: 'white',
-  width: '30%',
-  'text-align': 'center',
-  height: '250px'
-}
-const lWindow = {
-  background: '#737272',
-  color: 'white',
-  border: 'orange solid 2px',
-  width: '200px',
-  'text-align': 'center',
-  height: '75px'
-}
+
 class PaintShop extends Component {
 
   constructor(props) {
@@ -92,69 +38,48 @@ class PaintShop extends Component {
 }
 
   render() {
-    let time = moment().startOf('day').fromNow().split(" ")[0]
-    time = parseInt(time);
+    let time = parseInt(moment().format('k'),10);
     let timeOfDay = 7 < time && time < 17 ? daytime : nighttime;
     let owner = this.state.owner;
     let color = this.props.color;
     return (
-      <div className="" style={{'margin-left': '10%'}}>
+      <div className="" style={styles.offsetLeftStyle}>
         <div id="roof">
-          <div id="depth" style={{
-            // transform: 'rotateX(90deg) translateZ(1em)',
-            // position: 'absolute',
-            transform: 'skewX(-15deg)',
-            'margin-left': '.93%',
-            'border-radius': '10px 10px 0 0',
-            height: '5em',
-            width: '600px',
-            'text-align': 'center',
-            'line-height': '2em',
+          <div style={{
+            ...styles.slantedBlockStyle,
             background: `repeating-linear-gradient(to right, black, black 50px, ${color} 50px, ${color} 100px)`
           }}>
           </div>
           <div style={{
-            ...block,
-            transform: 'translateZ(1em)',
+            ...styles.verticalBlockStyle,
             background: `repeating-linear-gradient(to right, black, black 50px, ${color} 50px, ${color} 100px)`
           }}>
           </div>
         </div>
         <div>
-          <div style={{
-            background: 'url("https://image.freepik.com/free-photo/brick-wall-texture-background_1137-89.jpg")',
-            margin: '0 2%',
-            height: '30%',
-            width: '550px'
-          }}>
-            <div style={{height: '30', 'margin-top': '2%',}}>
+          <div style={styles.brickWallStyle}>
+            <div style={styles.spacerStyle}>
             </div>
-            <div className="row" style={{...row,'display': 'flex'}}>
-              <div style={{...update, ...timeOfDay}}><p>Paints</p></div>
-              <div style={{...update, ...timeOfDay}}><p>Brushes</p></div>
-              <div style={{...update, ...timeOfDay}}><p>Tape</p></div>
+            <div style={{...styles.rowStyle,'display': 'flex'}}>
+              <div style={{...styles.updateStyle, ...timeOfDay}}><p>Paints</p></div>
+              <div style={{...styles.updateStyle, ...timeOfDay}}><p>Brushes</p></div>
+              <div style={{...styles.updateStyle, ...timeOfDay}}><p>Tape</p></div>
             </div>
-            <div style={{height: '30', 'margin-top': '2%',}}>
+            <div style={styles.spacerStyle}>
             </div>
-            <div className="row" style={{...row,'display': 'flex'}}>
-              <div style={door}></div>
-              <div style={{...lWindow, ...timeOfDay}}>
+            <div style={{...styles.rowStyle,'display': 'flex'}}>
+              <div style={styles.doorStyle}></div>
+              <div style={{...styles.lWindowStyle, ...timeOfDay}}>
                 <h3>{`${owner}'s Paint Shop`}</h3>
               </div>
             </div>
          </div>
        </div>
-       <div style={{
-         'margin-left': '20%',
-         border: 'solid black 2px',
-         'text-align': 'center',
-         width: '300px'
-       }}>
+       <div style={styles.formStyle}>
          <h4>{`Now selling ${color} paint`}</h4>
          <div>
            <p>Change Paint <select
              name="paintChanger"
-             id=""
              onChange={this.changePaint}
              >
                <option value="red">Red</option>
@@ -169,7 +94,6 @@ class PaintShop extends Component {
               label="Change Owner"
               id="owner"
               placeholder='Store Owner'
-              type="text"
               autoComplete='off'
               value={this.state.owner}
               onChange={this.handleChange}
---
+++ src/components/paintShopStyle.js
@@ -0,0 +1,76 @@
+export const offsetLeftStyle = {
+  marginLeft: '10%'
+}
+
+export const slantedBlockStyle = {
+  transform: 'skewX(-15deg)',
+  marginLeft: '.93%',
+  borderRadius: '10px 10px 0 0',
+  height: '5em',
+  width: '600px',
+  textAlign: 'center',
+  lineHeight: '2em'
+}
+export const verticalBlockStyle = {
+  position: 'absolute',
+  height: '2em',
+  width: '600px',
+  textAlign: 'center',
+  lineHeight: '2em',
+  transform: 'translateZ(1em)'
+}
+
+export const brickWallStyle = {
+  background: 'url("https://image.freepik.com/free-photo/brick-wall-texture-background_1137-89.jpg")',
+  margin: '0 2%',
+  height: '30%',
+  width: '550px'
+}
+
+export const rowStyle = {
+  margin: '10px',
+  fontFamily: 'Lato, sans-serif',
+  marginLeft: '10%',
+  width: '400px',
+  justifyContent: 'space-between'
+}
+
+export const updateStyle = {
+  color: 'white',
+  border: 'orange solid 2px',
+  width: '25%',
+  padding: '1%',
+  textAlign: 'center',
+  height: '40px'
+}
+
+export const doorStyle = {
+  background: 'url("http://dc428.4shared.com/img/-vayshJ-/s3/ClassDoor.png")',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center center',
+  color: 'white',
+  width: '30%',
+  textAlign: 'center',
+  height: '250px'
+}
+
+export const lWindowStyle = {
+  background: '#737272',
+  color: 'white',
+  border: 'orange solid 2px',
+  width: '200px',
+  textAlign: 'center',
+  height: '75px'
+}
+
+export const formStyle = {
+  marginLeft: '20%',
+  border: 'solid black 2px',
+  textAlign: 'center',
+  width: '300px'
+}
+
+export const spacerStyle = {
+  height: '30px',
+  marginTop: '2%'
+}
